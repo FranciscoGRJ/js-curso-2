f9cf0825d6dc691761783a18fa4974dd3e99ccbe
Jose-sudo-star <jfranciscogromero@gmail.com>
2025-02-24 17:39:29 -0600

    Practice

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+function consoleButton () {
+console.log("El botton fue clicado")
+}
 
+function promptButton () {
+    const city = prompt("Ingrese el nombre de una ciudad")
+    alert(`Estuve en ${city} y me acorde de tí`)
+}
+
+function alertButton ( ) {
+    alert("Yo amo JS!")
+}
+
+function sum () {
+    valueA = prompt("Ingrese A")
+    valueB = prompt("Ingrese B")
+    alert(`La suma de A y B es ${parseInt(valueA) + parseInt(valueB)}`)
+}
+
+let title = document.querySelector("h1")
+title.innerHTML= "Hora del desafio"
